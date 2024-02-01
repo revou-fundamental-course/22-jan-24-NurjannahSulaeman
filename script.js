6ca9d90b371363139bd54c57e81259f5cba112c4
NurjannahSulaeman <156358176+NurjannahSulaeman@users.noreply.github.com>
2024-02-01 23:59:48 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function replaceName () {
+    let name = prompt ("Ketik Di Sini", "Produk-Produk");
+    document.getElementById("name"). innerHTML = name;
+}
+
+replaceName();
+
+console.log(replaceName)
